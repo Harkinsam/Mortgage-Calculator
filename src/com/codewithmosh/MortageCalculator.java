c44cb4bbe946aa57edf7f647fbc6a09b76794a1d
Akinrinade Samson <harkinsam247@gmail.com>
2024-03-09 12:45:18 +0100

    first commit

--- src/com/codewithmosh/MortageCalculator.java
+++ src/com/codewithmosh/MortageCalculator.java
@@ -4,20 +4,40 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class MortageCalculator {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        int principal;
+        do {
+            System.out.print("Principal: ");
+            principal = scanner.nextInt();
+            if (principal < 1000 || principal > 100000) {
+                System.out.println("Enter a number between 1000 and 1000000");
+            }
+        } while (principal < 1000 || principal > 1000000);
 
-        System.out.print("Annual Interest Rate: ");
-        float interest_Rate = scanner.nextFloat();
+        float interest_Rate;
+        do {
+            System.out.print("Annual Interest Rate: ");
+            interest_Rate = scanner.nextFloat();
+            if (interest_Rate < 1 || interest_Rate > 30) {
+                System.out.println("Enter a value greater than 0 and less than or equal to 30");
+            }
+        } while (interest_Rate < 1 || interest_Rate > 30);
+
         interest_Rate = interest_Rate / 1200;
 
-        System.out.print("period (Years): ");
-        int years = scanner.nextInt();
+        int years;
+        do{
+            System.out.print("period (Years): ");
+            years = scanner.nextInt();
+            if(years < 1 || years > 30){
+                System.out.println("Enter a value between 1 and 30");
+            }
+        }while(years < 1 || years >30);
         years = years * 12;
 
-        double mortgage = principal * (interest_Rate * Math.pow(1 + interest_Rate, years )) / Math.pow(1 + interest_Rate, years) - 1;
+
+        double mortgage = principal * (interest_Rate * Math.pow(1 + interest_Rate, years)) / Math.pow(1 + interest_Rate, years) - 1;
         NumberFormat currency = NumberFormat.getCurrencyInstance();
         String mortgagesCurrency = currency.format(mortgage);
         System.out.println("Mortgage: " + mortgagesCurrency);
